18ee06072a0b922c95cbad712448b7685232c38e
Vadim Tyukov <brainreflex@gmail.com>
2013-11-29 22:08:21 +0200

    [quick-fix] Adds missed phpdoc’s

--- src/Vatson/Callback/Ipc/IpcInterface.php
+++ src/Vatson/Callback/Ipc/IpcInterface.php
@@ -5,7 +5,15 @@ namespace Vatson\Callback\Ipc;
 
 interface IpcInterface
 {
+    /**
+     * @return mixed
+     */
     public function get();
 
+    /**
+     * @param mixed $data
+     *
+     * @return void
+     */
     public function put($data);
 }
